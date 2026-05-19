4c2cd9ec53fa190fed7713deb1278b55eb8a23ad
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2026-05-19 11:38:49 +0200

    chore: Cleanup

--- Sources/InfomaniakCore/Account/UserProfile/UserProfileStore.swift
+++ Sources/InfomaniakCore/Account/UserProfile/UserProfileStore.swift
@@ -40,7 +40,8 @@ public actor UserProfileStore {
     }
 
     @discardableResult
-    public func updateUserProfile(with apiFetcher: ApiFetcher, options: ProfileWithOptionSet = [.emails, .phones]) async throws -> UserProfile {
+    public func updateUserProfile(with apiFetcher: ApiFetcher,
+                                  options: ProfileWithOptionSet = [.emails, .phones]) async throws -> UserProfile {
         await loadIfNeeded()
         let user = try await apiFetcher.userProfile(ignoreDefaultAvatar: true, dateFormat: .iso8601, with: options)
         await addUserProfile(user)

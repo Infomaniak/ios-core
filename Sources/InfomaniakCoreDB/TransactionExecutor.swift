ab3aaff97369238156d787bb4d202554b477f3ae
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-06-22 10:36:12 +0200

    fix: Revert to compatibility with other project
    Method add in extention on kDrive

--- Sources/InfomaniakCoreDB/TransactionExecutor.swift
+++ Sources/InfomaniakCoreDB/TransactionExecutor.swift
@@ -24,7 +24,7 @@ import RealmSwift
 ///
 ///  Only write transactions are protected from sudden termination, will extend to read if required.
 public struct TransactionExecutor: Transactionable {
-    let realmAccessible: RealmAccessible
+    public let realmAccessible: RealmAccessible
 
     public init(realmAccessible: RealmAccessible) {
         self.realmAccessible = realmAccessible
@@ -63,14 +63,12 @@ public struct TransactionExecutor: Transactionable {
     }
 
     public func writeTransaction(withRealm realmClosure: (Realm) throws -> Void) throws {
-        try writeTransaction(withExpiringActivity: true, withRealm: realmClosure)
-    }
-
-    public func writeTransaction(withExpiringActivity enabled: Bool, withRealm realmClosure: (Realm) throws -> Void) throws {
         try autoreleasepool {
-            let expiringActivity: ExpiringActivity? = enabled ? ExpiringActivity() : nil
-            expiringActivity?.start()
-            defer { expiringActivity?.endAll() }
+            let expiringActivity = ExpiringActivity()
+            expiringActivity.start()
+            defer {
+                expiringActivity.endAll()
+            }
 
             let realm = realmAccessible.getRealm()
             try realm.safeWrite {
